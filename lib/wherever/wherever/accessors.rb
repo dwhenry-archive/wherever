b76f4cb7c57dc93ed46533fcfddad7fcd3a4c84b
David Henry <dw_henry@yahoo.com.au>
2011-10-30 01:15:52 +0000

    add some debugging details

--- lib/wherever/wherever/accessors.rb
+++ lib/wherever/wherever/accessors.rb
@@ -3,6 +3,10 @@ class Wherever
     def get_key_store(*keys)
       mark = keys.last.is_a?(Hash) ? keys.pop["marker"] : marker
       collection(mark).stores.find_or_create_by(:key => keys)
+    rescue => e
+      p e.inspect
+      p mark
+      p keys
     end
     
     def collection(mark=marker)
