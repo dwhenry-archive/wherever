ec7f48fe010e7117f89ced2795f8aad5481074c7
David Henry <dw_henry@yahoo.com.au>
2011-11-04 15:36:40 +0000

    clean up the collection copy code a little

--- lib/wherever/wherever/mark.rb
+++ lib/wherever/wherever/mark.rb
@@ -1,19 +1,23 @@
 class Wherever
   module Mark
     def mark(name)
-      configuration = if Mongoid::Config.respond_to?(:master) 
-        Mongoid::Config
-      else
-        Mongoid::Config.instance
-      end
-      configuration.master.eval("db.current_identifier.find().forEach( function(x){db.#{name}_identifier.insert(x)} )")
+      copy_collections("current_identifier", "#{name}_identifier")
       config.key_groups.each do |keys|
         [:identifier, :dataset].each do |db_type|
           db_id = "#{keys.join('_')}_#{db_type}"
-          configuration.master.eval("db.current_#{db_id}.find().forEach( function(x){db.#{name}_#{db_id}.insert(x)} )")
+          copy_collections("current_#{db_id}", "#{name}_#{db_id}")
         end
       end
       set_price_lookup('price', nil, [{'marker' => name}])
+    end
+    
+    def copy_collections(from_collection, to_collection)
+      copy_function = "db.#{from_collection}.find().forEach( function(x){db.#{to_collection}.insert(x)} )"
+      if Mongoid::Config.respond_to?(:instance)
+        Mongoid::Config.instance.master.eval(copy_function)
+      else
+        Mongoid::Config.master.eval(copy_function)
+      end
     end
   end
 end
